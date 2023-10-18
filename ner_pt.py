c0de57af99751188a752eaaeaba13ba57e1cced8
rgoliveirati <45003762+rgoliveirati@users.noreply.github.com>
2023-10-18 18:16:50 -0300

    Add files via upload

---
+++ ner_pt.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import spacy
+
+# Carrega o modelo spaCy para português
+nlp = spacy.load("pt_core_news_sm")
+
+# Título da aplicação
+st.title("Extrator de Entidades Nomeadas em Português")
+
+# Caixa de texto para inserir o texto
+input_text = st.text_area("Insira o texto em português:")
+
+# Função para extrair as entidades
+def extract_entities(text):
+    doc = nlp(text)
+    entities = [(ent.text, ent.label_) for ent in doc.ents]
+    return entities
+
+# Botão para executar a extração de entidades
+if st.button("Extrair Entidades"):
+    if input_text:
+        entities = extract_entities(input_text)
+        if entities:
+            st.subheader("Entidades Nomeadas Encontradas:")
+            for entity, label in entities:
+                st.write(f"Entidade: {entity}, Tipo: {label}")
+        else:
+            st.write("Nenhuma entidade encontrada no texto inserido.")
+    else:
+        st.write("Por favor, insira um texto para realizar a extração de entidades.")
+
+# Nota informativa sobre o modelo utilizado
+st.write("Este aplicativo utiliza o modelo 'pt_core_news_sm' do spaCy para NER em português.")
